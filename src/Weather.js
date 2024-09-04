1c7de66f8292d99c5ac41c0ad6adc86398052bbc
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-09-04 22:18:19 +0100

    Fixed error

--- src/Weather.js
+++ src/Weather.js
@@ -62,7 +62,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast data={weatherData} />
       </div>
     );
   } else {
