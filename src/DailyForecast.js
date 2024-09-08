333f4732862369a0a27ecc43e7fc39d6cd9845df
shajmin306 <157534142+shajmin306@users.noreply.github.com>
2024-09-08 11:21:16 +0100

    Corrected daily forecast

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 export default function DailyForecast(props) {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let date = days[new Date(props.data.time * 1000).getDay()];
+
   return (
     <div>
-      <div className="WeatherForecastDay">{props.data.time}</div>
-      <img src={props.data.iconUrl} alt={props.data.description} />
+      <div className="WeatherForecastDay">{date}</div>
+      <img
+        src={props.data.condition.icon_url}
+        alt={props.data.condition.description}
+      />
       <div className="WeatherForecastTemperatures">
         <span className="WeatherForecastMinTemp">
-          {Math.round(props.data.temp.min)}°C|
+          {Math.round(props.data.temperature.minimum)}°C |{" "}
         </span>
         <span className="WeatherForecastMaxTemp">
-          {Math.round(props.data.temp.max)}°C{" "}
+          {Math.round(props.data.temperature.maximum)}°C{" "}
         </span>
       </div>{" "}
     </div>
